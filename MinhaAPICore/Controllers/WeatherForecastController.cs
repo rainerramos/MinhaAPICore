b60d3bc53df48145706500a53d781ea0af51825d
rainerramos <rainermr89@hotmail.com>
2022-08-11 15:46:20 -0300

    Rotas

--- MinhaAPICore/Controllers/WeatherForecastController.cs
+++ MinhaAPICore/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace MinhaAPICore.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("obter-por-id/{id}")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
